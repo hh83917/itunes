d5180a0631fa1021b50e2f781529e6f04c611b92
Howard Huang <hh83917@mac.com>
2015-11-06 09:08:14 -0600

    Completed itunes project

--- js/itunesService.js
+++ js/itunesService.js
@@ -6,9 +6,33 @@ app.service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+  this.getData = function(artist) {
+      var defered = $q.defer();
+      $http({
+        method: 'JSONP',
+        url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+      }).then(function(response) {
+        var sData = response.data.results;
+        // console.log(sData);
+        var songArray = [];
+        for (var i = 0; i < sData.length; i++) {
+          var songObj = {
+            Play: sData[i].previewUrl,
+            Artist: sData[i].artistName,
+            Collection: sData[i].collectionName,
+            AlbumArt: sData[i].artworkUrl100,
+            Type: sData[i].kind,
+            CollectionPrice: sData[i].collectionPrice,
+            TrackName: sData[i].trackName
+          };
+          songArray.push(songObj);
+        }
+        defered.resolve(songArray);
+      });
+      return defered.promise;
+  };
 });
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,13 +4,13 @@ app.controller('mainCtrl', function($scope, itunesService){
   //This is setting up the default behavior of our ng-grid. The important thing to note is
   //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
   //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
-  $scope.gridOptions = { 
+  $scope.gridOptions = {
       data: 'songData',
       height: '110px',
       sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
       columnDefs: [
         {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
-        {field: 'Artist', displayName: 'Artist'},
+        {field: 'TrackName', displayName: 'Track Name'}, {field: 'Artist', displayName: 'Artist'},
         {field: 'Collection', displayName: 'Collection'},
         {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
         {field: 'Type', displayName: 'Type'},
@@ -25,11 +25,19 @@ app.controller('mainCtrl', function($scope, itunesService){
     //code here
 
 
+
   //Now write a function that will call the method on the itunesService that is responsible for getting the data from iTunes, whenever the user clicks the submit button
   //*remember, that method should be expecting an artist name. The artist name is coming from the input box on index.html, head over there and check if that input box is tied to any specific model we could use.
   //Also note that that method should be retuning a promise, so you could use .then in this function.
-    
+
     //Code here
+    $scope.getSongData = function() {
+      itunesService.getData($scope.artist)
+      .then(function(dataFromService) {
+        $scope.songData = dataFromService;
+        // console.log(dataFromService);
+      });
+    };
 
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
@@ -56,7 +64,3 @@ app.controller('mainCtrl', function($scope, itunesService){
 
     //Code here
 });
-
-
-
-
